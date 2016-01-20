b22b512810d025c47cd1f96d6b233bbc7d6e3344
tolo137 <matthewj.egan@hotmail.com>
2016-01-20 15:39:21 +1100

    Added skeleton code to begin CLI

---
+++ THAssistant.py
@@ -0,0 +1,10 @@
+## THAssistant.py
+##
+## Written by Matthew Egan - January 2016
+##
+## A class that helps process different calculations for 
+## a home game poker tournament.
+
+class THAssistant:
+    def __init__(self):
+        pass
---
+++ THmain.py
@@ -0,0 +1,10 @@
+## THmain.py
+##
+## Written by Matthew Egan - January 2016
+##
+## The main function for the CL version of TourneyHelper
+
+def main():
+    pass
+
+if __name__ == "__main__": main()
