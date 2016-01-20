13757f7c869fcb92b70a5d5d23b2f702fda42ebb
tolo137 <matthewj.egan@hotmail.com>
2016-01-20 16:47:34 +1100

    Began on prizepool distributions, uniform dist complete

--- THAssistant.py
+++ THAssistant.py
@@ -5,6 +5,53 @@
 ## A class that helps process different calculations for 
 ## a home game poker tournament.
 
+import math
+
 class THAssistant:
-    def __init__(self):
+    def __init__(self, players, prizepool, payable):
+        self.players = int(players)
+        self.prizepool = int(prizepool)
+        self.payablePlayers = int(payable)
+
+        self.distFuncs = {"uniform" : self.uniform,
+                          "linear" : self.linear,
+                          "geometric" : self.geometric,
+                          "lognormal" : self.lognormal,
+                          "exponential" : self.exponential}
+
+    def prizeDist(self, dist):
+        if isinstance(dist, str):
+            if dist in self.distFuncs:
+                return self.distFuncs[dist]()
+            else:
+                print "ValueError: dist not a valid value"
+                print "Please choose [linear, quadratic, hyperbolic, exponential]"
+        else:
+            print "TypeError: dist is not a string"
+
+    def uniform(self):
+        ## All the same, prizepool/payable
+        val = self.prizepool/self.payablePlayers
+        remaining = self.prizepool
+        table = []
+
+        for p in xrange(self.payablePlayers):
+            table.append(val)
+            remaining -= val
+        table[0] += remaining
+        
+        return table
+
+    def linear(self):
+        ## y = 1  - (1/players)position
+        pass
+
+    def geometric(self):
+        ## y = 1/(2^position)
+        pass
+
+    def lognormal(self):
+        pass
+
+    def exponential(self):
         pass
--- THmain.py
+++ THmain.py
@@ -1,10 +1,19 @@
+#!/usr/bin/python
+##
 ## THmain.py
 ##
 ## Written by Matthew Egan - January 2016
 ##
 ## The main function for the CL version of TourneyHelper
 
+from THAssistant import THAssistant
+
 def main():
-    pass
+    director = THAssistant(10, 100, 3)
+    total = 0
+    for e, pos in enumerate(director.prizeDist("uniform")):
+        print e, ":", pos
+        total += pos
+    print "Total:", total
 
 if __name__ == "__main__": main()
