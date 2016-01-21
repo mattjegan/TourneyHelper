25ee0a95136d46887754efcb54ad19820742a6cf
tolo137 <matthewj.egan@hotmail.com>
2016-01-21 14:02:25 +1100

    Added nicer CLI

--- THmain.py
+++ THmain.py
@@ -9,9 +9,31 @@
 from THAssistant import THAssistant
 
 def main():
-    director = THAssistant(100, 1000, 7)
+    print "Welcome to TourneyHelper"
+    print "What would you like to do?"
+    print "\t1 - Calculate Prize Pool Distribution"
+    command = int(raw_input())
+
+    {1:ppDist}[command]()
+
+def ppDist():
+
+    players = int(raw_input("How many players are there: "))
+    prizepool = int(raw_input("What is the prizepool: "))
+    paidPos = int(raw_input("How many paid positions would you like: "))
+
+    print "What distribution would you like?"
+    print "\t1 - Uniform"
+    print "\t2 - Geometric"
+    print "\t3 - Log-Normal"
+    print "\t4 - Exponential"
+    distType = int(raw_input())
+
+    director = THAssistant(players, prizepool, paidPos)
     total = 0
-    for e, pos in enumerate(director.prizeDist("exponential")):
+    print ""
+    print "Prize Pool Distribution:"
+    for e, pos in enumerate(director.prizeDist({1:"uniform", 2:"geometric", 3:"lognormal", 4:"exponential"}[distType])):
         print e+1, ":", pos
         total += pos
     print "Total:", total
